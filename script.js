ba431bd52adbf553c6f213f5897d65cc6684af8e
Patan Khan <patan_khan@bonify.de>
2023-08-29 23:15:41 +0530

    Added Hurray page

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ const ruleButton = document.getElementById("ruleButton")
 
 const gameRuleCloseIcon = document.getElementById("gameRuleCloseIcon")
 
+const playAgain = document.getElementById("playagain");
+
 ruleButton.addEventListener("click", () => {
     const gamedisplay = gameRuleCard.style.display;
-    if(gamedisplay !== "block") {
-        gameRuleCard.style.display="block"
+    if (gamedisplay !== "block") {
+        gameRuleCard.style.display = "block"
     }
 })
 
 gameRuleCloseIcon.addEventListener("click", () => {
-    gameRuleCard.style.display="none"
+    gameRuleCard.style.display = "none"
 })
 
+playAgain.addEventListener("click", () => {
+    window.location.href = "index.html"
+})
